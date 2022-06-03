164ce39086d32bd1c2c0f2d69de54f9d0b826863
Boris <stealer91119@gmail.com>
2022-06-03 18:44:08 +0300

    03.06.2022

--- js/script.js
+++ js/script.js
@@ -20,9 +20,87 @@ const movieDB = {
         "Лига справедливости",
         "Ла-ла лэнд",
         "Одержимость",
-        "Скотт Пилигрим против..."
+        "Скотт Пилигрим против...",
+        "Адом"
     ]
 };
 
-console.log('hello');
+
+
+const adv = document.querySelectorAll('.promo__adv img'); // получить рекламных блоков
+
+const genre =document.querySelector('.promo__genre'); // получить элемент с жанром 
+
+const bg = document.querySelector('.promo__bg');
+
+const interactiveIist = document.querySelector('.promo__interactive-list');
+
+
+
+// удаление рекламных блоков
+// Изменить жанр фильма, поменять "комедия" на "драма"
+function init(){
+    adv.forEach(el => {
+        el.remove();
+    });
+
+    genre.textContent = 'драма';
+
+    bg.style.backgroundImage = 'url("img/bg.jpg")';
+
+    
+    movieDB.movies.sort();
+}
+
+init();
+
+interactiveIist.innerHTML = '';
+
+
+// 
+function renderInteractiveIist(){
+
+    getPost('http://localhost:3000/movies')
+        .then(post => {
+            post.sort();
+            post.forEach((el,i) =>{
+                interactiveIist.innerHTML += `
+                    <li class="promo__interactive-item">${i+1}. ${el}
+                        <div class="delete"></div>
+                    </li>
+                `;
+            });
+        });
+
+}
+
+renderInteractiveIist();
+
+// function getPost(url){
+//     const request = new XMLHttpRequest();
+
+//     request.open('GET', url);
+//     request.setRequestHeader('Content-type', 'application/json; charset:utf-8');
+//     request.send();
+
+//     request.addEventListener('load', () =>{
+//         if(request.status == 200){
+//             console.log(request.response);
+//         } else{
+//             console.log('error');
+//         }
+//     });
+// }
+
+async function getPost(url){
+    const post = await fetch(url);
+    return await post.json();
+}
+
+
+
+
+
+console.log('here');
+
 
